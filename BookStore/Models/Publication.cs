ea67f4f88bef22aaa34a56db16c1b95d70f7e2de
Mads Hougesen <madshougesen@gmail.com>
2021-09-20 18:52:40 +0200

    feat: Publication model

---
+++ BookStore/Models/Publication.cs
@@ -0,0 +1,13 @@
+﻿namespace BookStore.Models
+{
+    public class Publication
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Publisher { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
